5dbc834028c6b00c1173ce5b8c7155e21b18a631
Abhishek Anand <120253524+anand-1Abhishek@users.noreply.github.com>
2024-10-11 01:59:18 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const dotenv = require('dotenv');
 const app = express();
 dotenv.config(); 
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 
